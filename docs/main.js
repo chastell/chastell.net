37fdedf02f05ad0f009fc843d3fab8a5d054fef7
Piotr Szotkowski <chastell@chastell.net>
2018-10-06 18:39:11 +0000

    rebuild

---
+++ docs/main.js
@@ -0,0 +1,36 @@
+document.addEventListener('keydown', function(event) {
+  if (event.altKey || event.ctrlKey) return;
+  const id = Number(window.location.hash.substring(1)) || 0;
+  let target;
+  switch (event.key) {
+    case 'ArrowLeft':
+    case 'h':
+      document.getElementsByClassName('next')[0].click();
+      return;
+    case 'ArrowRight':
+    case 'l':
+      document.getElementsByClassName('previous')[0].click();
+      return;
+    case 'ArrowUp':
+    case 'PageUp':
+    case 'k':
+      target = id - 1;
+      break;
+    case 'ArrowDown':
+    case 'PageDown':
+    case 'j':
+      target = id + 1;
+      break;
+    case 'End':
+    case 'Home':
+      history.replaceState(null, null, '#');
+      return;
+    default:
+      return;
+  }
+  const element = document.getElementById(target);
+  if (!element) return;
+  event.preventDefault();
+  element.scrollIntoView({ behavior: 'smooth' });
+  history.replaceState(null, null, '#' + target);
+});
