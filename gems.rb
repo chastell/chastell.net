d5cec448f2ec4ad239208d80c00c9ac068fb7a45
Piotr Szotkowski <chastell@chastell.net>
2019-09-03 00:40:18 +0200

    Bump jekyll

--- gems.rb
+++ gems.rb
@@ -2,6 +2,6 @@ source 'https://rubygems.org'
 
 gem 'exifr',     '~> 1.2'
 gem 'fastimage', '~> 2.1'
-gem 'jekyll',    '~> 3.8'
+gem 'jekyll',    '~> 4.0'
 gem 'rake',      '~> 12.3'
 gem 't',         '~> 3.1'
